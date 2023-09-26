c4ba9337bbc960769b7959463b15fbdff4f1b0b6
naveen bharadwaj <bharadwajpal22@gmail.com>
2023-09-26 12:08:31 +0530

    update in text

--- lib/main.dart
+++ lib/main.dart
@@ -54,7 +54,7 @@ class _MyHomePageState extends State<MyHomePage> {
               foundation.kReleaseMode ? "PROD" : "SANDBOX",
             ),
             const Text(
-              'You have pushed the button this many times.:',
+              'Bonjour You have pushed the button this many times.:',
             ),
             Text(
               '$_counter',
